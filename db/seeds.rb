6f1d980d89d8279d7cdbe2e31bfccbf0585fb7a4
du halgouet <duhalgouetm@yahoo.com>
2018-05-29 17:10:17 +0200

    ajout du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+environment_seed_file = File.join(Rails.root, 'db', 'seeds', "#{Rails.env}.rb")
+
+def seed_image(file_name)
+    File.open(File.join(Rails.root, "/app/assets/images/seed/#{file_name}.jpg"))
+end
+
+
+Liste_chat= [
+{:title => 'chat1', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat1.png'},
+{:title => 'chat2', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat2.png'},
+{:title => 'chat3', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat3.png'},
+{:title => 'chat4', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images.chat4.png'},
+{:title => 'chat5', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat5.png'},
+{:title => 'chat6', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat6.png'},
+{:title => 'chat7', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat7.png'},
+{:title => 'chat8', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat8.png'},
+{:title => 'chat9', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat9.png'},
+{:title => 'chat10', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat10.png'},
+{:title => 'chat11', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat1.png'},
+{:title => 'chat12', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat2.png'},
+{:title => 'chat13', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat3.png'},
+{:title => 'chat14', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images.chat4.png'},
+{:title => 'chat15', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat5.png'},
+{:title => 'chat16', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat6.png'},
+{:title => 'chat17', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat7.png'},
+{:title => 'chat18', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat8.png'},
+{:title => 'chat19', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat9.png'},
+{:title => 'chat20', :description => 'lorem ipsum', :price => 10, :image_url => 'app/assets/images/chat10.png'},
+
+]
+
+Item.delete_all
+
+Liste_chat.each do |chat|
+    chats = Item.create(chat)
+end
